e7daa32003cb4388f86e4bd7379878e8158ea016
Natan H. Pereira <xxnatanll@hotmail.com>
2024-12-12 15:54:50 -0300

    Update 4.c

--- 4.c
+++ 4.c
@@ -1,31 +1,27 @@
 #include<stdio.h>
 
-int par(int x, int y, int z){
+int par(int x[]){
+
     int qtdPar = 0;
 
-        if(x % 2 == 0)
+    for(int i = 0; i < 3; i++){
+        if(x[i] % 2 == 0)
         {
             qtdPar += 1;
         }
-        if(y % 2 == 0)
-        {
-            qtdPar += 1;
-        }
-        if(z % 2 == 0)
-        {
-            qtdPar += 1;
-        }
-
+    }
     return qtdPar;
 }
 
 int main(){
 
-    int a, b, c;
+    int x[3];
 
-    scanf("%d %d %d", &a, &b, &c);
+    for(int i = 0; i < 3; i++){
+        scanf("%d", &x[i]);
+    }
 
-    printf("Quantidade de numeros par eh: %d\n", par(a, b, c));
+    printf("Quantidade de numeros par eh: %d", par(x));
 
     return 0;
 }
