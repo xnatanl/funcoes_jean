81d2ff0bcbf042c22abb65c1c90452090410ddbf
Natan H. Pereira <xxnatanll@hotmail.com>
2024-12-12 15:41:27 -0300

    Add files via upload

---
+++ 11.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+int divisores(int n){
+
+    for(int i = 1; i <= n; i++){
+        if(n%i==0){
+            printf("%d\n", i);
+        }
+    }
+}
+
+int main(){
+
+    int n;
+
+    scanf("%d", &n);
+
+    divisores(n);
+
+    return 0;
+}
